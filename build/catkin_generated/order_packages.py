3284ee13c07d1d009de341d905ffb55db15f08ca
yukitsuji <yukitsuji020832@gmail.com>
2017-02-25 19:32:57 +0900

    add

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/katou01/BagDataConverter/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/katou01/BagDataConverter/devel;/opt/ros/indigo".split(';') if "/home/katou01/BagDataConverter/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/usr/local/cuda-7.5/lib64:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib"
-export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/indigo/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/cuda-7.5:/usr/local/cuda-7.5/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/katou01/BagDataConverter/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib:/lib/x86_64-linux-gnu:/usr/local/cuda-8.0/lib64:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/indigo/bin:/usr/local/cuda-8.0:/usr/local/cuda-8.0/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 export PWD="/home/katou01/BagDataConverter/build"
+export PYTHONPATH="/opt/ros/indigo/lib/python2.7/dist-packages:/home/katou01/caffe/python"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/katou01/BagDataConverter/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/katou01/BagDataConverter/src:$ROS_PACKAGE_PATH"
