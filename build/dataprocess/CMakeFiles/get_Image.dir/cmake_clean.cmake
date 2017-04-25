bf6cc5784222223b36b1f935e60c1739cc346bc4
yukitsuji <yukitsuji020832@gmail.com>
2017-04-25 21:05:56 +0900

    Add cpp

---
+++ build/dataprocess/CMakeFiles/get_Image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/get_Image.dir/src/get_Image.cpp.o"
+  "/home/katou01/BagDataConverter/devel/lib/dataprocess/get_Image.pdb"
+  "/home/katou01/BagDataConverter/devel/lib/dataprocess/get_Image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/get_Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/dataprocess/CMakeFiles/get_PCD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/get_PCD.dir/src/get_PCD.cpp.o"
+  "/home/katou01/BagDataConverter/devel/lib/dataprocess/get_PCD.pdb"
+  "/home/katou01/BagDataConverter/devel/lib/dataprocess/get_PCD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/get_PCD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
